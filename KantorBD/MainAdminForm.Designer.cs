b16d403bc217eab8490a6b96f8c583ed7eb0747d
Marcin020 <marcinduda729@gmail.com>
2024-04-22 22:09:11 +0200

    Początek menu dla administratora

--- KantorBD/MainAdminForm.Designer.cs
+++ KantorBD/MainAdminForm.Designer.cs
@@ -28,14 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainAdminForm));
             panel1 = new Panel();
             labelClose = new Label();
+            MenuPanel = new Panel();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
             panel1.SuspendLayout();
+            MenuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(36, 26, 82);
+            panel1.Controls.Add(MenuPanel);
             panel1.Controls.Add(labelClose);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
@@ -56,18 +66,65 @@
             labelClose.Text = "X";
             labelClose.Click += labelClose_Click;
             // 
-            // MainForm
+            // MenuPanel
+            // 
+            MenuPanel.BackColor = Color.FromArgb(16, 9, 48);
+            MenuPanel.Controls.Add(pictureBox3);
+            MenuPanel.Controls.Add(pictureBox2);
+            MenuPanel.Controls.Add(pictureBox1);
+            MenuPanel.Location = new Point(0, 64);
+            MenuPanel.Name = "MenuPanel";
+            MenuPanel.Size = new Size(1064, 110);
+            MenuPanel.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(16, 9, 48);
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.ImageLocation = "";
+            pictureBox1.Location = new Point(985, 20);
+            pictureBox1.Margin = new Padding(5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(65, 65);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(910, 20);
+            pictureBox2.Margin = new Padding(5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(65, 65);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(835, 20);
+            pictureBox3.Margin = new Padding(5);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(65, 65);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // MainAdminForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1064, 681);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "MainForm";
+            Name = "MainAdminForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainForm";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            MenuPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
         }
 
@@ -75,5 +132,9 @@
 
         private Panel panel1;
         private Label labelClose;
+        private Panel MenuPanel;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
     }
 }
