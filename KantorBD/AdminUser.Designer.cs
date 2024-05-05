b2ca79548f314262b019e794836bd627b78a817c
Marcin020 <marcinduda729@gmail.com>
2024-05-05 23:04:42 +0200

    Dodanie panelu do zarządzania użytkownikami (graficzny)

--- KantorBD/AdminUser.Designer.cs
+++ KantorBD/AdminUser.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminUser));
             panel1 = new Panel();
+            label2 = new Label();
+            dataGridViewUser = new DataGridView();
             label1 = new Label();
             MenuPanel = new Panel();
             pictureBox6 = new PictureBox();
@@ -39,7 +41,27 @@
             pictureBox2 = new PictureBox();
             pictureBox1 = new PictureBox();
             labelClose = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            numericID = new NumericUpDown();
+            textBoxName = new TextBox();
+            textBoxSurname = new TextBox();
+            textBoxEmail = new TextBox();
+            textBoxBirth = new TextBox();
+            textBoxUsertype = new TextBox();
+            buttonSearch = new Button();
+            buttonAdd = new Button();
+            buttonEdit = new Button();
+            buttonRemove = new Button();
+            buttonFirst = new Button();
+            buttonPrevious = new Button();
+            buttonNext = new Button();
+            buttonLast = new Button();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUser).BeginInit();
             MenuPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
@@ -47,11 +69,33 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericID).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(36, 26, 82);
+            panel1.Controls.Add(buttonLast);
+            panel1.Controls.Add(buttonNext);
+            panel1.Controls.Add(buttonPrevious);
+            panel1.Controls.Add(buttonFirst);
+            panel1.Controls.Add(buttonRemove);
+            panel1.Controls.Add(buttonEdit);
+            panel1.Controls.Add(buttonAdd);
+            panel1.Controls.Add(buttonSearch);
+            panel1.Controls.Add(textBoxUsertype);
+            panel1.Controls.Add(textBoxBirth);
+            panel1.Controls.Add(textBoxEmail);
+            panel1.Controls.Add(textBoxSurname);
+            panel1.Controls.Add(textBoxName);
+            panel1.Controls.Add(numericID);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(dataGridViewUser);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(MenuPanel);
             panel1.Controls.Add(labelClose);
@@ -60,6 +104,25 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(1064, 681);
             panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label2.ForeColor = Color.FromArgb(255, 193, 37);
+            label2.Location = new Point(52, 503);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 29);
+            label2.TabIndex = 20;
+            label2.Text = "ID";
+            // 
+            // dataGridViewUser
+            // 
+            dataGridViewUser.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewUser.Location = new Point(12, 180);
+            dataGridViewUser.Name = "dataGridViewUser";
+            dataGridViewUser.Size = new Size(1040, 243);
+            dataGridViewUser.TabIndex = 19;
             // 
             // label1
             // 
@@ -171,6 +234,212 @@
             labelClose.Text = "X";
             labelClose.Click += labelClose_Click;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label3.ForeColor = Color.FromArgb(255, 193, 37);
+            label3.Location = new Point(179, 503);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 29);
+            label3.TabIndex = 21;
+            label3.Text = "NAME";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label4.ForeColor = Color.FromArgb(255, 193, 37);
+            label4.Location = new Point(339, 503);
+            label4.Name = "label4";
+            label4.Size = new Size(116, 29);
+            label4.TabIndex = 22;
+            label4.Text = "SURNAME";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label5.ForeColor = Color.FromArgb(255, 193, 37);
+            label5.Location = new Point(535, 503);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 29);
+            label5.TabIndex = 23;
+            label5.Text = "E-MAIL";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label6.ForeColor = Color.FromArgb(255, 193, 37);
+            label6.Location = new Point(712, 503);
+            label6.Name = "label6";
+            label6.Size = new Size(143, 29);
+            label6.TabIndex = 24;
+            label6.Text = "BIRTH DATE";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label7.ForeColor = Color.FromArgb(255, 193, 37);
+            label7.Location = new Point(874, 503);
+            label7.Name = "label7";
+            label7.Size = new Size(168, 29);
+            label7.TabIndex = 25;
+            label7.Text = "USER_TYPE_ID";
+            // 
+            // numericID
+            // 
+            numericID.Location = new Point(32, 539);
+            numericID.Name = "numericID";
+            numericID.Size = new Size(71, 23);
+            numericID.TabIndex = 26;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Font = new Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBoxName.Location = new Point(131, 539);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(170, 35);
+            textBoxName.TabIndex = 27;
+            // 
+            // textBoxSurname
+            // 
+            textBoxSurname.Font = new Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBoxSurname.Location = new Point(309, 539);
+            textBoxSurname.Name = "textBoxSurname";
+            textBoxSurname.Size = new Size(170, 35);
+            textBoxSurname.TabIndex = 28;
+            // 
+            // textBoxEmail
+            // 
+            textBoxEmail.Font = new Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBoxEmail.Location = new Point(485, 539);
+            textBoxEmail.Name = "textBoxEmail";
+            textBoxEmail.Size = new Size(195, 35);
+            textBoxEmail.TabIndex = 29;
+            // 
+            // textBoxBirth
+            // 
+            textBoxBirth.Font = new Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBoxBirth.Location = new Point(702, 539);
+            textBoxBirth.Name = "textBoxBirth";
+            textBoxBirth.Size = new Size(163, 35);
+            textBoxBirth.TabIndex = 30;
+            // 
+            // textBoxUsertype
+            // 
+            textBoxUsertype.Font = new Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBoxUsertype.Location = new Point(901, 539);
+            textBoxUsertype.Name = "textBoxUsertype";
+            textBoxUsertype.Size = new Size(122, 35);
+            textBoxUsertype.TabIndex = 31;
+            // 
+            // buttonSearch
+            // 
+            buttonSearch.BackColor = Color.FromArgb(16, 9, 48);
+            buttonSearch.FlatStyle = FlatStyle.Flat;
+            buttonSearch.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            buttonSearch.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonSearch.Location = new Point(197, 609);
+            buttonSearch.Name = "buttonSearch";
+            buttonSearch.Size = new Size(150, 50);
+            buttonSearch.TabIndex = 32;
+            buttonSearch.Text = "Search";
+            buttonSearch.UseVisualStyleBackColor = false;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.BackColor = Color.FromArgb(16, 9, 48);
+            buttonAdd.FlatStyle = FlatStyle.Flat;
+            buttonAdd.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            buttonAdd.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonAdd.Location = new Point(371, 609);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(150, 50);
+            buttonAdd.TabIndex = 33;
+            buttonAdd.Text = "Add";
+            buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // buttonEdit
+            // 
+            buttonEdit.BackColor = Color.FromArgb(16, 9, 48);
+            buttonEdit.FlatStyle = FlatStyle.Flat;
+            buttonEdit.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            buttonEdit.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonEdit.Location = new Point(543, 609);
+            buttonEdit.Name = "buttonEdit";
+            buttonEdit.Size = new Size(150, 50);
+            buttonEdit.TabIndex = 34;
+            buttonEdit.Text = "Edit";
+            buttonEdit.UseVisualStyleBackColor = false;
+            // 
+            // buttonRemove
+            // 
+            buttonRemove.BackColor = Color.FromArgb(16, 9, 48);
+            buttonRemove.FlatStyle = FlatStyle.Flat;
+            buttonRemove.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            buttonRemove.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonRemove.Location = new Point(710, 609);
+            buttonRemove.Name = "buttonRemove";
+            buttonRemove.Size = new Size(150, 50);
+            buttonRemove.TabIndex = 35;
+            buttonRemove.Text = "Remove";
+            buttonRemove.UseVisualStyleBackColor = false;
+            // 
+            // buttonFirst
+            // 
+            buttonFirst.BackColor = Color.FromArgb(16, 9, 48);
+            buttonFirst.FlatStyle = FlatStyle.Flat;
+            buttonFirst.Font = new Font("Comic Sans MS", 22.5F, FontStyle.Bold);
+            buttonFirst.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonFirst.Location = new Point(197, 429);
+            buttonFirst.Name = "buttonFirst";
+            buttonFirst.Size = new Size(150, 50);
+            buttonFirst.TabIndex = 36;
+            buttonFirst.Text = "<<";
+            buttonFirst.UseVisualStyleBackColor = false;
+            // 
+            // buttonPrevious
+            // 
+            buttonPrevious.BackColor = Color.FromArgb(16, 9, 48);
+            buttonPrevious.FlatStyle = FlatStyle.Flat;
+            buttonPrevious.Font = new Font("Comic Sans MS", 22.5F, FontStyle.Bold);
+            buttonPrevious.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonPrevious.Location = new Point(371, 429);
+            buttonPrevious.Name = "buttonPrevious";
+            buttonPrevious.Size = new Size(150, 50);
+            buttonPrevious.TabIndex = 37;
+            buttonPrevious.Text = "<";
+            buttonPrevious.UseVisualStyleBackColor = false;
+            // 
+            // buttonNext
+            // 
+            buttonNext.BackColor = Color.FromArgb(16, 9, 48);
+            buttonNext.FlatStyle = FlatStyle.Flat;
+            buttonNext.Font = new Font("Comic Sans MS", 22.5F, FontStyle.Bold);
+            buttonNext.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonNext.Location = new Point(543, 429);
+            buttonNext.Name = "buttonNext";
+            buttonNext.Size = new Size(150, 50);
+            buttonNext.TabIndex = 38;
+            buttonNext.Text = ">";
+            buttonNext.UseVisualStyleBackColor = false;
+            // 
+            // buttonLast
+            // 
+            buttonLast.BackColor = Color.FromArgb(16, 9, 48);
+            buttonLast.FlatStyle = FlatStyle.Flat;
+            buttonLast.Font = new Font("Comic Sans MS", 22.5F, FontStyle.Bold);
+            buttonLast.ForeColor = Color.FromArgb(255, 193, 37);
+            buttonLast.Location = new Point(710, 429);
+            buttonLast.Name = "buttonLast";
+            buttonLast.Size = new Size(150, 50);
+            buttonLast.TabIndex = 39;
+            buttonLast.Text = ">>";
+            buttonLast.UseVisualStyleBackColor = false;
+            // 
             // AdminUser
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -183,6 +452,7 @@
             Text = "AdminUser";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUser).EndInit();
             MenuPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
@@ -190,6 +460,7 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericID).EndInit();
             ResumeLayout(false);
         }
 
@@ -205,5 +476,26 @@
         private Label labelClose;
         private PictureBox pictureBox6;
         private Label label1;
+        private Label label2;
+        private DataGridView dataGridViewUser;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private TextBox textBoxName;
+        private NumericUpDown numericID;
+        private TextBox textBoxUsertype;
+        private TextBox textBoxBirth;
+        private TextBox textBoxEmail;
+        private TextBox textBoxSurname;
+        private Button buttonSearch;
+        private Button buttonRemove;
+        private Button buttonEdit;
+        private Button buttonAdd;
+        private Button buttonLast;
+        private Button buttonNext;
+        private Button buttonPrevious;
+        private Button buttonFirst;
     }
 }
