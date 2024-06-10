d5e9681b7713e6f4693ee6023a0602e41e708505
Jakub Jan Ćwik <115538640+MocarnyDyktator69@users.noreply.github.com>
2024-06-10 18:19:27 +0200

    Reapply "Dodanie jobs do aktualzizcji kursów"
    This reverts commit 16ecf37bae5cc52c3a67c2ec65597ee072e5f952.

---
+++ KantorBD/Jobs/ExchangeRateService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KantorBD
+{
+    public class ExchangeRateService
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private string apiUrl = "https://v6.exchangerate-api.com/v6/b9f4c7090a7d9ee239826cc5/latest/";
+
+        public async Task<string> GetExchangeRatesAsync(string fromCurrency)
+        {
+            HttpResponseMessage response = await client.GetAsync(apiUrl + fromCurrency);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
